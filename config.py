f24b3f17fb05d7b26d1673c396225a963fa87fc7
David Lewis <meng.lewis@gmail.com>
2014-05-02 22:31:45 +0000

    Refactored code that missed the previous commit

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-
-SECRET_KEY = "devtestkey"
-
-#CELERY_BROKER_URL = ""
-#CELERY_RESULT_BACKEND = ""
